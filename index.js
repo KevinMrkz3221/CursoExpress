459f6c7e0c740bec7c97c73d7c67be4d8d0b6286
Kevin Andres Rosales Marquez <kevin.rosales98@hotmail.com>
2021-10-13 15:01:09 -0600

    Se agrego faker y se implemetaron parametros por query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const faker = require('faker')
+
 const app = express();
 
 const port = 3000;
@@ -12,9 +14,52 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  const products = [];
+
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    });
+  }
+
+  res.json(products);
+});
+
+app.get('/categories', (req, res) =>{
+  res.json([
+    {
+      category: "Blusa"
+    },
+    {
+      category: "camisa"
+    },
+    {
+      category: "pantalon"
+    }
+  ]);
+});
+
+app.get('/products/:id', (req, res) =>{
+  const { id } = req.params;
+
   res.json({
-    name: "Producto 1",
-    price: 1200
+    id,
+    name: "product 2",
+    price: 200
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+
+  res.json({
+    categoryId,
+    productId
   });
 });
 
