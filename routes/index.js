e5a53d31d682127fbe871774a67ae74786cc307b
Kevin Andres Rosales Marquez <kevin.rosales98@hotmail.com>
2021-10-13 18:56:26 -0600

    Se crearon servicios

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 
 const productsRouter = require('./products.router');
-const categoriesRouter = require('./categories.router')
-const usersRouter = require('./users.router')
-const testsRouter = require('./test.router')
+const categoriesRouter = require('./categories.router');
+const usersRouter = require('./users.router');
+const testsRouter = require('./test.router');
+const orderRouter = require('./orders.router');
 
 
 function routerApi(app){
   const router = express.Router();
-  
+
   app.use('/api/v1', router);
   app.use('/', testsRouter);
 
   router.use('/products', productsRouter);
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
+  router.use('/oders', orderRouter);
 }
 
 
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,22 +1,13 @@
 const express = require('express');
-const faker = require('faker')
+const ProductService = require('../services/product.service');
 
 
 const router = express.Router();
+const service = new ProductService();
 
 router.get('/', (req, res) => {
-  const products = [];
+  const products = service.find();
 
-  const { size } = req.query;
-  const limit = size || 10;
-
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl()
-    });
-  }
   res.json(products);
 });
 
@@ -26,20 +17,36 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) =>{
   const { id } = req.params;
-
-  res.json({
-    id,
-    name: "product 2",
-    price: 200
-  });
+  const product = service.findOne(id);
+  res.json(product);
 });
 
 router.post("/", (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     messaje: "created",
     data: body
   })
+});
+
+router.patch("/:id", (req, res) => {
+  const body = req.body;
+  const { id } = req.params;
+
+  res.json({
+    messaje: "update",
+    data: body,
+    id,
+  })
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    message: 'deleted',
+    id
+  });
 });
 
 module.exports = router;
